25f7e3b9294c274c3b2fcbd1584e2de10332c0fd
Yosua <godbless_yosua@yahoo.co.id>
2024-07-07 21:58:41 +0700

    remove cache

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,18 +1,9 @@
 import { SERVICE_INFO, ServiceMode } from "@/constants";
 import { SongDb } from "@/types";
 
-function getDbKey(mode: ServiceMode): string {
-  return `SONG_DB_${mode}`;
-}
-
 export async function loadSongDb(mode: ServiceMode): Promise<SongDb> {
-  const key = getDbKey(mode);
-  let result = localStorage.getItem(key);
-  if (!result) {
-    const res = await fetch(SERVICE_INFO[mode].songDb);
-    result = await res.text();
-    localStorage.setItem(key, result);
-  }
+  const res = await fetch(SERVICE_INFO[mode].songDb);
+  const result = await res.text();
   return JSON.parse(result);
 }
 
--- src/utils/template.ts
+++ src/utils/template.ts
@@ -1,16 +1,7 @@
 import { TEMPLATE_KEY } from "@/constants";
 
-function getKey(template: TEMPLATE_KEY): string {
-  return `TEMPLATE_${template}`;
-}
-
 export async function loadTemplate(template: TEMPLATE_KEY): Promise<string> {
-  const key = getKey(template);
-  let result = localStorage.getItem(key);
-  if (!result) {
-    const res = await fetch(`/templates/${template}`);
-    result = await res.text();
-    localStorage.setItem(key, result);
-  }
+  const res = await fetch(`/templates/${template}`);
+  const result = await res.text();
   return result;
 }

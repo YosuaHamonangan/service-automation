bc71026c130e3947e6268359aa9afffa0bf99191
Yosua <godbless_yosua@yahoo.co.id>
2024-07-13 21:05:39 +0700

    fix bible query bug

--- src/utils/ppt.ts
+++ src/utils/ppt.ts
@@ -83,14 +83,21 @@ function addTextSlide(pptx: PptxGenJS, title: string, content: string) {
 async function getAlkitabtext(mode: ServiceMode, info: AlkitabInfo) {
   const { book, chapter, verses } = info;
   const db = await loadAlkitabDb(mode, ALKITAB_INFO[mode].indexOf(book) + 1);
-  const chapterText = db[+chapter - 1];
+  const verseList = db[+chapter - 1].map((str) => {
+    const result = str.match(/.+:([0-9]+)([a-z]?) /);
+    const verse = result?.[1] ?? "";
+    const subverse = result?.[2];
+    return { str, verse, subverse };
+  });
 
   const result: string[] = [];
   verses.split(",").forEach((v) => {
-    const verse = chapterText[+v - 1];
-    if (!verse) return;
+    const matchedVerses = verseList.filter(({ verse }) => verse === v);
+    if (!matchedVerses) return;
 
-    result.push(verse);
+    matchedVerses.forEach(({ str }) => {
+      result.push(str);
+    });
   });
   return result;
 }
